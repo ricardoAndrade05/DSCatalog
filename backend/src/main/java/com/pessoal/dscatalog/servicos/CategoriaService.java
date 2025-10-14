8394b1ea0ad62fd2f058b9b111c3667fe7136ea3
ricardo.andrade <ricardo.andrade05@gmail.com>
2025-10-13 21:19:02 -0300

    Transações e sessão JPA,Seeding da base de dados

--- backend/src/main/java/com/pessoal/dscatalog/servicos/CategoriaService.java
+++ backend/src/main/java/com/pessoal/dscatalog/servicos/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.pessoal.dscatalog.entidades.Categoria;
 import com.pessoal.dscatalog.repositorios.CategoriaRepository;
@@ -14,6 +15,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Categoria> findAll() {
 		List<Categoria> categorias = repository.findAll();
 		return categorias;
