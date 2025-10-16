eacedce453b0eb9f29234e2f8cdae27eff753beb
ricardo.andrade <ricardo.andrade05@gmail.com>
2025-10-15 22:28:49 -0300

    Paginação

--- backend/src/main/java/com/pessoal/dscatalog/controladores/CategoriaController.java
+++ backend/src/main/java/com/pessoal/dscatalog/controladores/CategoriaController.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,8 +28,8 @@ public class CategoriaController {
 	private CategoriaService service;
 
 	@GetMapping
-	public ResponseEntity<List<CategoriaDTO>> categorias() {
-		List<CategoriaDTO> list = service.categorias();
+	public ResponseEntity<Page<CategoriaDTO>> categorias(Pageable pageable) {
+		Page<CategoriaDTO> list = service.categorias(pageable);
 		return ResponseEntity.ok().body(list);
 	}
 
--- backend/src/main/java/com/pessoal/dscatalog/servicos/CategoriaService.java
+++ backend/src/main/java/com/pessoal/dscatalog/servicos/CategoriaService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,9 +25,9 @@ public class CategoriaService {
 	private CategoriaRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<CategoriaDTO> categorias() {
-		List<Categoria> categorias = repository.findAll();
-		return categorias.stream().map(categoria -> new CategoriaDTO(categoria)).toList();
+	public Page<CategoriaDTO> categorias(Pageable pageable) {
+		Page<Categoria> page = repository.findAll(pageable);
+		return page.map(categoria -> new CategoriaDTO(categoria));
 	}
 
 	@Transactional(readOnly = true)
