e1aecc4cadfdb1f2318019f4ee9ddc39f93f943f
ricardo.andrade <ricardo.andrade05@gmail.com>
2025-10-15 21:45:29 -0300

    Dados de auditoria

--- backend/src/main/java/com/pessoal/dscatalog/entidades/Categoria.java
+++ backend/src/main/java/com/pessoal/dscatalog/entidades/Categoria.java
@@ -1,11 +1,15 @@
 package com.pessoal.dscatalog.entidades;
 
+import java.time.Instant;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,6 +20,12 @@ public class Categoria {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant dataCriacao;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant dataAtualizacao;
 	
 	public Categoria() {
 	}
@@ -40,7 +50,25 @@ public class Categoria {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
+	
+	public Instant getDataCriacao() {
+		return dataCriacao;
+	}
+	
+	public Instant getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		dataCriacao = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		dataAtualizacao = Instant.now();
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO tb_categoria (nome) VALUES ('Livros');
-INSERT INTO tb_categoria (nome) VALUES ('Eletrônicos');
-INSERT INTO tb_categoria (nome) VALUES ('Computadores');
+INSERT INTO tb_categoria (nome, data_criacao) VALUES ('Livros',now());
+INSERT INTO tb_categoria (nome, data_criacao) VALUES ('Eletrônicos',now());
+INSERT INTO tb_categoria (nome, data_criacao) VALUES ('Computadores',now());
 
 --INSERT INTO tb_product (name, price, date, description, img_url) VALUES ('The Lord of the Rings', 90.5, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg');
 --INSERT INTO tb_product (name, price, date, description, img_url) VALUES ('Smart TV', 2190.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg');
