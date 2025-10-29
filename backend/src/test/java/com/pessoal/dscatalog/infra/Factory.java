3c2f0671ee9a35ba2bdad3c5280d6573fc7f8aea
ricardo.andrade <ricardo.andrade05@gmail.com>
2025-10-28 23:38:34 -0300

    Resolução exercicios

--- backend/src/test/java/com/pessoal/dscatalog/infra/Factory.java
+++ backend/src/test/java/com/pessoal/dscatalog/infra/Factory.java
@@ -11,12 +11,16 @@ public class Factory {
 	
 	public static Produto criarProduto() {
 		Produto produto = new Produto(1L, "Computador", "Bom computador", 1200.0,Instant.parse("2020-10-20T03:00:00Z"), "https://img.com/img.png");
-		produto.getCategorias().add(new Categoria(2L, "Eletrônicos"));
+		produto.getCategorias().add(criarCategoria());
 		return produto;
 	}
 	
 	public static ProdutoDTO criarProdutoDTO() {
 		Produto produto = criarProduto();
 		return new ProdutoDTO(produto, produto.getCategorias());
+	}
+
+	public static Categoria criarCategoria() {
+		return new Categoria(2L, "Eletrônicos");
 	}
 }
--- backend/src/test/java/com/pessoal/dscatalog/servicos/ProdutoServiceTests.java
+++ backend/src/test/java/com/pessoal/dscatalog/servicos/ProdutoServiceTests.java
@@ -18,10 +18,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.pessoal.dscatalog.dto.ProdutoDTO;
+import com.pessoal.dscatalog.entidades.Categoria;
 import com.pessoal.dscatalog.entidades.Produto;
 import com.pessoal.dscatalog.infra.Factory;
 import com.pessoal.dscatalog.infra.excecoes.DatabaseException;
 import com.pessoal.dscatalog.infra.excecoes.RecursoNaoEncontradoException;
+import com.pessoal.dscatalog.repositorios.CategoriaRepository;
 import com.pessoal.dscatalog.repositorios.ProdutoRepository;
 
 @ExtendWith(SpringExtension.class)
@@ -32,13 +34,17 @@ public class ProdutoServiceTests {
 	private static Long ID_COM_DEPENDENCIA;
 	private static PageImpl<Produto> page;
 	private static Produto produto;
-	
+	private static Categoria categoria;
+	private static ProdutoDTO produtoDTO;
 
 	@InjectMocks
 	private ProdutoService service;
 
 	@Mock
 	private ProdutoRepository repository;
+	
+	@Mock
+	private CategoriaRepository categoriaRepository;
 
 	@BeforeEach()
 	void setUp() throws Exception {
@@ -46,9 +52,11 @@ public class ProdutoServiceTests {
 		ID_INEXISTENTE = 1000L;
 		ID_COM_DEPENDENCIA = 4L;
 		produto = Factory.criarProduto();
+		categoria = Factory.criarCategoria();
+		produtoDTO = Factory.criarProdutoDTO();
 		page = new PageImpl<>(List.of(produto));
-		
-		Mockito.when(repository.findAll((Pageable)ArgumentMatchers.any())).thenReturn(page);
+
+		Mockito.when(repository.findAll((Pageable) ArgumentMatchers.any())).thenReturn(page);
 		Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(produto);
 		Mockito.when(repository.findById(ID_EXISTENTE)).thenReturn(Optional.of(produto));
 		Mockito.when(repository.findById(ID_INEXISTENTE)).thenReturn(Optional.empty());
@@ -58,6 +66,13 @@ public class ProdutoServiceTests {
 		Mockito.when(repository.existsById(ID_COM_DEPENDENCIA)).thenReturn(true);
 		Mockito.doThrow(RecursoNaoEncontradoException.class).when(repository).deleteById(ID_INEXISTENTE);
 		Mockito.doThrow(DatabaseException.class).when(repository).deleteById(ID_COM_DEPENDENCIA);
+
+		Mockito.when(repository.getReferenceById(ID_EXISTENTE)).thenReturn(produto);
+		Mockito.doThrow(RecursoNaoEncontradoException.class).when(repository).getReferenceById(ID_INEXISTENTE);
+		
+		Mockito.when(categoriaRepository.getReferenceById(ID_EXISTENTE)).thenReturn(categoria);
+		Mockito.when(categoriaRepository.getReferenceById(ID_INEXISTENTE)).thenThrow(RecursoNaoEncontradoException.class);
+
 	}
 
 	@Test
@@ -83,12 +98,43 @@ public class ProdutoServiceTests {
 		});
 		Mockito.verify(repository, Mockito.times(1)).deleteById(ID_COM_DEPENDENCIA);
 	}
-	
+
 	@Test
 	public void produtosDeveRetornarPaginaDeProdutos() {
 		Pageable pageable = PageRequest.of(0, 10);
 		Page<ProdutoDTO> pageProdutos = service.produtos(pageable);
 		Assertions.assertNotNull(pageProdutos);
 		Mockito.verify(repository, Mockito.times(1)).findAll(pageable);
+	}
+
+	@Test
+	public void produtoPorIdDeveRetornarProdutoDTOQuandoIdExistir() {
+		ProdutoDTO dto = service.produtoPorId(ID_EXISTENTE);
+		Assertions.assertNotNull(dto);
+		Mockito.verify(repository, Mockito.times(1)).findById(ID_EXISTENTE);
+	}
+
+	@Test
+	public void produtoPorIdDeveLancarRecursoNaoEncontradoExceptionQuandoIdNaoExistir() {
+		Assertions.assertThrows(RecursoNaoEncontradoException.class, () -> {
+			service.produtoPorId(ID_INEXISTENTE);
+		});
+		Mockito.verify(repository, Mockito.times(1)).findById(ID_INEXISTENTE);
+	}
+
+	@Test
+	public void atualizarDeveRetornarProdutoDTOQuandoIdExistir() {
+		ProdutoDTO dto = service.atualizar(ID_EXISTENTE, produtoDTO);
+		Assertions.assertNotNull(dto);
+		Mockito.verify(repository, Mockito.times(1)).getReferenceById(ID_EXISTENTE);
+		Mockito.verify(repository, Mockito.times(1)).save(ArgumentMatchers.any());
+	}
+
+	@Test
+	public void atualizarDeveLancarRecursoNaoEncontradoExceptionQuandoIdNaoExistir() {
+		Assertions.assertThrows(RecursoNaoEncontradoException.class, () -> {
+			service.atualizar(ID_INEXISTENTE, produtoDTO);
+		});
+		Mockito.verify(repository, Mockito.times(1)).getReferenceById(ID_INEXISTENTE);
 	}
 }
