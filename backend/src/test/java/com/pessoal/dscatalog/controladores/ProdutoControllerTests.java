08114ab3ea5360baefbcd5a3bfc8ec36c706d211
ricardo.andrade <ricardo.andrade05@gmail.com>
2025-11-03 22:24:22 -0300

    Testando o findById

--- backend/src/test/java/com/pessoal/dscatalog/controladores/ProdutoControllerTests.java
+++ backend/src/test/java/com/pessoal/dscatalog/controladores/ProdutoControllerTests.java
@@ -2,6 +2,7 @@ package com.pessoal.dscatalog.controladores;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.springframework.http.MediaType;
@@ -19,31 +20,55 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import com.pessoal.dscatalog.dto.ProdutoDTO;
 import com.pessoal.dscatalog.infra.Factory;
+import com.pessoal.dscatalog.infra.excecoes.RecursoNaoEncontradoException;
 import com.pessoal.dscatalog.servicos.ProdutoService;
 
 @WebMvcTest(ProdutoController.class)
 public class ProdutoControllerTests {
 	
+	private PageImpl<ProdutoDTO> page;
+	private ProdutoDTO produtoDTO;
+	private Long idExistente;
+	private Long idInexistente;
+	
+	
 	@Autowired
 	private MockMvc mockMvc;
 	
-	private PageImpl<ProdutoDTO> page;
-	private ProdutoDTO produtoDTO;
-
 	@MockitoBean
 	private ProdutoService service;
 	
 	@BeforeEach()
 	void setUp() throws Exception {
+		idExistente = 1L;
+		idInexistente = 1000L;
 		produtoDTO = Factory.criarProdutoDTO();
 		page = new PageImpl<>(List.of(produtoDTO));
 		
 		when(service.produtos(ArgumentMatchers.any())).thenReturn(page);
+		when(service.produtoPorId(idExistente)).thenReturn(produtoDTO);
+		when(service.produtoPorId(idInexistente)).thenThrow(RecursoNaoEncontradoException.class);
 	}
 	
 	@Test
 	public void listarProdutosDeveriaRetornarPaginaDeProdutos() throws Exception {
 		ResultActions result = mockMvc.perform(get("/produtos").accept(MediaType.APPLICATION_JSON)); 
 		result.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void buscarProdutoPorIdDeveriaRetornarProdutoQuandoIdExistir() throws Exception {
+		ResultActions result = mockMvc.perform(get("/produtos/{id}", idExistente).accept(MediaType.APPLICATION_JSON)); 
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.id").exists());
+		result.andExpect(jsonPath("$.nome").exists());
+		result.andExpect(jsonPath("$.descricao").exists());
+	}
+	
+	@Test
+	public void buscarProdutoPorIdDeveriaRetornar404QuandoIdNaoExistir() throws Exception {
+		ResultActions result = mockMvc.perform(get("/produtos/{id}", idInexistente).accept(MediaType.APPLICATION_JSON)); 
+		result.andExpect(status().isNotFound());
+		
 	}
 }
